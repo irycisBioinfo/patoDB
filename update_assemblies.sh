577bf207abe7a9aa38c027aebfcd3524d7ef6a10
valflanza <valfernandez.vf@gmail.com>
2022-02-23 11:03:20 +0100

    update feb 23 2022

--- update_assemblies.sh
+++ update_assemblies.sh
@@ -14,3 +14,30 @@ echo -e 'assembly_accession\tspecies_taxid\torganism_name' > header_class.tsv
 
 cut -f 1,7,8 tmp >> header_class.tsv
 
+mkdir faa
+mkdir fna
+
+tar xvf genome_assemblies_genome_fasta.tar -C ./fna
+tar xvf genome_assemblies_prot_fasta.tar -C ./faa
+
+ls $PWD/faa/*/*.gz > list_faa 
+ls $PWD/fna/*/*.gz > list_fna
+
+mash sketch -l list_fna -p 20 -o ReferenceNucl.msh
+mash sketch -l list_faa -a -p 20 -o ReferenceProt.msh 
+
+
+mkdir toTar
+mv VFDB_set* toTar/
+mv resfinder_* toTar/
+mv headers* toTar/
+mv annot.data toTar/
+mv bacmet* toTar/
+mv header_class.tsv toTar/
+mv ReferenceNucl.msh toTar/
+mv ReferenceProt.msh toTar/
+
+tar -czvf patodb.tar.gz ./toTar/*
+
+# clean the folder and commit to github
+
